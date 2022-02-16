0390a528ba6749dc232929516d5098870a5b2ff7
SantiagoVillalba <67521266+SantiagoVillalba@users.noreply.github.com>
2022-02-16 00:04:57 -0300

    pog

--- Reto/mysite/mysite/settings.py
+++ Reto/mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-UY'
 
-TIME_ZONE = 'UTC-3'
+TIME_ZONE = 'America/Montevideo'
 
 USE_I18N = True
 
--- Reto/mysite/polls/admin.py
+++ Reto/mysite/polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Question, Football
+
+admin.site.register(Question)
+
+
+
+
+
+
+
+
+
+admin.site.register(Football)
 # Register your models here.
